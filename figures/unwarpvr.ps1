808687d0f172fe47d71645dd08263fb3e439ef85
Anthony Elliott <ajellio2@ncsu.edu>
2014-11-18 11:07:36 -0500

    Add equipment images on first page.

--- figures/unwarpvr.ps1
+++ figures/unwarpvr.ps1
@@ -1,8 +1,8 @@
-$unwarpvr_path = "C:\Users\Brian\Program Files\Oculus\DK2\ffmpeg-unwarpvr.exe"
-$unwarp_options = "unwarpvr=1280:720:left_eye_only=1:scale_width=1.2:scale_height=1.2"
+$unwarpvr_path = "C:\bin\ffmpeg-unwarpvr-0.2-Windows\ffmpeg-unwarpvr.exe"
+$unwarp_options = "unwarpvr=1280:720:left_eye_only=1:scale_width=1.3:scale_height=0.7"
 
-$source_image = "stack.png"
-$dest_image = "unwarpped_stack.png"
+$source_image = "webcam_passthrough.png"
+$dest_image = "unwarped_webcam_passthrough.png"
 & $unwarpvr_path -i "$source_image" -vf $unwarp_options -c:a copy -f image2 "$dest_image"
 
 # $source_video = "source.flv"
