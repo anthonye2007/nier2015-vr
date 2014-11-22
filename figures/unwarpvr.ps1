23ed4a46155a7dc078bab5a09254c85ff4ddeb86
Anthony Elliott <ajellio2@ncsu.edu>
2014-11-21 21:03:23 -0500

    Improve Immersion screenshot

--- figures/unwarpvr.ps1
+++ figures/unwarpvr.ps1
@@ -1,8 +1,8 @@
 $unwarpvr_path = "C:\bin\ffmpeg-unwarpvr-0.2-Windows\ffmpeg-unwarpvr.exe"
-$unwarp_options = "unwarpvr=1280:720:left_eye_only=1:scale_width=1.3:scale_height=0.7"
+$unwarp_options = "unwarpvr=1280:720:left_eye_only=1:scale_width=1.2:scale_height=0.7"
 
-$source_image = "webcam_passthrough.png"
-$dest_image = "unwarped_webcam_passthrough.png"
+$source_image = "immersion.png"
+$dest_image = "immersion_unwarped3.png"
 & $unwarpvr_path -i "$source_image" -vf $unwarp_options -c:a copy -f image2 "$dest_image"
 
 # $source_video = "source.flv"
